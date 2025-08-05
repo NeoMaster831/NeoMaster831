d0395024044605fef1e07256350f947068a6ebc2
wane <me@wane.im>
2025-08-05 10:51:21 +0000

    test pgp

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+with open('PGP_KEY', 'r') as f:
+    pgp_key = f.read()
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/pgp')
+def pgp():
+    return pgp_key
